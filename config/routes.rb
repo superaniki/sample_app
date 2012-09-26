9731c5fa9fcb751fc81a35d2a4a6119c73932fd8
Rickard Sandgren <rickardsandgren@gmail.com>
2012-09-26 15:15:36 +0200

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy] # dont use show or edit
 
   root to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete # invode http DELETE request
   match '/signup', to: 'users#new'
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
