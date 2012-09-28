0621912f2028278ad01f22aeb2db9b5878b05ddd
Rickard Sandgren <rickardsandgren@gmail.com>
2012-09-28 13:56:54 +0200

    Finish lotsa actions!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
+
+  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
   def new
     @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+    
+    
+  end
+  
+  def update
+    
+      if @user.update_attributes(params[:user])
+        flash[:success] = "Profile Updated"
+        sign_in @user
+        redirect_to @user
+        ## we need to sign in again
+      end
   end
   
   def create
@@ -17,4 +38,33 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:success] =  "User #{@user.name} has been deleted."
+    redirect_to users_url
+  end
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in? # =
+        # flash[:notice] = "Please sign in."
+        # redirect_to signin_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
+    
+  
 end
